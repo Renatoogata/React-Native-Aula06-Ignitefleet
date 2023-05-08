9577208445d00de2900086412f3359f503324188
Renato Ide Ogata <renatoogata95@gmail.com>
2023-05-08 17:38:09 -0300

    HomeHeader funcs, logout, user info

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,25 +1,38 @@
 import { TouchableOpacity } from 'react-native';
 import { Power } from 'phosphor-react-native'
+import { useUser, useApp } from '@realm/react';
 
 import theme from '@theme/index';
 
-import { Container, Greeting, Message, Name } from "./styles";
+import { Container, Greeting, Message, Name, Picture } from "./styles";
 
 
 export function HomeHeader() {
+    const user = useUser(); // pegando dados do usuário autenticado com o google
+    const app = useApp();
+
+    function handleLogout() {
+        app.currentUser?.logOut();
+    }
+
     return (
         <Container>
+            <Picture
+                source={{ uri: user?.profile.pictureUrl }}
+                placeholder="L184i9kCW=of00ayjZay~qj[ayj@" // blurhash
+            />
+
             <Greeting>
                 <Message>
                     Olá
                 </Message>
 
                 <Name>
-                    Renato
+                    {user?.profile.name}
                 </Name>
             </Greeting>
 
-            <TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
                 <Power
                     size={32}
                     color={theme.COLORS.GRAY_400}
