967a7d7b932270cc810581c9cafddf96ac9a0e4d
Renato Ide Ogata <renatoogata95@gmail.com>
2023-05-15 17:38:40 -0300

    Input Refs, Departure screen and TextAreaInput component

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,3 +1,6 @@
+import { useRef } from 'react';
+import { TextInput } from 'react-native';
+
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { LicensePlateInput } from '@components/LicensePlateInput';
@@ -6,6 +9,13 @@ import { TextAreaInput } from '@components/TextAreaInput';
 import { Container, Content } from './styles';
 
 export function Departure() {
+
+    const descriptionRef = useRef<TextInput>(null)
+
+    function handleDepartureRegister() {
+        console.log('OK!')
+    }
+
     return (
         <Container>
             <Header title='Saída' />
@@ -15,15 +25,22 @@ export function Departure() {
                 <LicensePlateInput
                     label='Placa do veículo'
                     placeholder='BRA1234'
+                    onSubmitEditing={() => descriptionRef.current?.focus()} // passar para o proximo input pelo teclado utilizando ref
+                    returnKeyType="next" // mudando o icone
                 />
 
                 <TextAreaInput
+                    ref={descriptionRef}
                     label='Finalidade'
                     placeholder='Vou utilizar o veículo para...'
+                    onSubmitEditing={handleDepartureRegister} // chamando a função com o teclado
+                    returnKeyType="send" // mudando o icone
+                    blurOnSubmit
                 />
 
                 <Button
                     title='Registrar Saída'
+                    onPress={handleDepartureRegister}
                 />
             </Content>
         </Container>
