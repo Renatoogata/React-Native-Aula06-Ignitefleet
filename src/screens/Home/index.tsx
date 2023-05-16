6d2fd3b2e223924f8be1142a929e649bdd66bd1d
Renato Ide Ogata <renatoogata95@gmail.com>
2023-05-16 16:07:52 -0300

    fetch vehicle in offline database

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
+
+import { useQuery } from '@libs/realm'
+import { Historic } from '@libs/realm/schemas/Historic'
 
 import { CarStatus } from '@components/CarStatus'
 import { HomeHeader } from '@components/HomeHeader'
 
+import { Alert } from 'react-native'
+
 import { Container, Content } from './styles'
 
 export function Home() {
+    const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+
     const { navigate } = useNavigation();
+
+    const historic = useQuery(Historic);
 
     function handleRegisterMovement() {
         navigate('departure');
     }
+
+    function fetchVehicle() {
+        try {
+            const vehicle = historic.filtered("status = 'departure'")[0] // filtar veículos que o status é departure [0](o primeiro)
+            setVehicleInUse(vehicle);
+        } catch (error) {
+            Alert.alert('Veículo em uso', 'Não foi possível carregar o veículo em uso.');
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchVehicle()
+    }, [])
 
     return (
         <Container>
@@ -18,6 +42,7 @@ export function Home() {
 
             <Content>
                 <CarStatus
+                    licensePlate={vehicleInUse?.license_plate}
                     onPress={handleRegisterMovement}
                 />
             </Content>
